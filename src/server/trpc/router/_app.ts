a55e6a5fca0bd0b80bb8aa1b0d40faec45191194
Sven <svenrisse0@gmail.com>
2022-12-10 16:55:27 +0100

    feat: add tweetRouter to router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,9 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
-import { exampleRouter } from "./example";
+import { tweetRouter } from "./tweet";
 
 export const appRouter = router({
-  example: exampleRouter,
+  tweet: tweetRouter,
   auth: authRouter,
 });
 
