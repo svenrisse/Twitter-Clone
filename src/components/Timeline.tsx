8686097f1104affa96ee699ea4e167179f783f3d
Sven <svenrisse0@gmail.com>
2022-12-11 00:06:33 +0100

    feat<timeline>: list tweets

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,10 +1,47 @@
+import Image from "next/image";
+import { RouterOutputs, trpc } from "../utils/trpc";
 import { CreateTweet } from "./CreateTweet";
 
-export function Timeline() {
+function Tweet({
+  tweet,
+}: {
+  tweet: RouterOutputs["tweet"]["timeline"]["tweets"][number];
+}) {
+  return (
+    <div className="mb-4 border-b-2 border-gray-500">
+      <div className="flex p-2">
+        {tweet.author.image && (
+          <Image
+            src={tweet.author.image}
+            alt={`${tweet.author.name} profile picture`}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        )}
+        <div className="ml-2">
+          <div className="flex">
+            <p>{tweet.author.name}</p>
+            <p> - {new Date(tweet.createdAt).toISOString()}</p>
+          </div>
 
-    return(
-        <div>
-            <CreateTweet />
+          <div>{tweet.text}</div>
         </div>
-    )
+      </div>
+    </div>
+  );
+}
+
+export function Timeline() {
+  const { data } = trpc.tweet.timeline.useQuery({ limit: 2 });
+  return (
+    <div>
+      <CreateTweet />
+      <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
+        {data?.tweets.map((tweet) => {
+          return <Tweet key={tweet.id} tweet={tweet} />;
+        })}
+      </div>
+    </div>
+  );
 }
