9b530dd10db3a4cf70b2881f706c6199f5ede0b0
Sven <svenrisse0@gmail.com>
2022-12-10 16:55:03 +0100

    refactor: use tweetSchema

--- src/server/trpc/router/tweet.ts
+++ src/server/trpc/router/tweet.ts
@@ -1,26 +1,21 @@
 import { z } from "zod";
+import { tweetSchema } from "../../../components/CreateTweet";
 import { protectedProcedure, router } from "../trpc";
 
 export const tweetRouter = router({
-  create: protectedProcedure
-    .input(
-      z.object({
-        text: z.string({ required_error: "Tweet is required" }),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      const { prisma, session } = ctx;
-      const { text } = input;
-      const userId = session.user.id;
-      return prisma.tweet.create({
-        data: {
-          text,
-          author: {
-            connect: {
-              id: userId,
-            },
+  create: protectedProcedure.input(tweetSchema).mutation(({ ctx, input }) => {
+    const { prisma, session } = ctx;
+    const { text } = input;
+    const userId = session.user.id;
+    return prisma.tweet.create({
+      data: {
+        text,
+        author: {
+          connect: {
+            id: userId,
           },
         },
-      });
-    }),
+      },
+    });
+  }),
 });
