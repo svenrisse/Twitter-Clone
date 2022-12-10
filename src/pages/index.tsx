e18bf3183a71b2a67acda25787f1a81869adc901
Sven <svenrisse0@gmail.com>
2022-12-10 16:52:28 +0100

    feat: add Timeline to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
+import { Timeline } from "../components/Timeline";
 
 const Home: NextPage = () => {
+
+  const {data: session} = useSession()
 
   return (
     <>
@@ -15,9 +18,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        Hello from trpc
-      </div>
+      <button onClick={() => signIn()}>Login</button>
+      <Timeline />
     </>
   );
 };
