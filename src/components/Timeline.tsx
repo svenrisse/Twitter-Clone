7fd20b3c83b5006ae576d246da35ad59f86fbc24
Sven <svenrisse0@gmail.com>
2022-12-12 13:51:38 +0100

    feat<timeline>: infinite scroll

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -2,6 +2,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import updateLocal from "dayjs/plugin/updateLocale";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { RouterOutputs, trpc } from "../utils/trpc";
 import { CreateTweet } from "./CreateTweet";
 
@@ -25,6 +26,33 @@ dayjs.updateLocale("en", {
     yy: "%dy",
   },
 });
+
+function useScrollPosition() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  function handleScroll() {
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const scrolled = (winScroll / height) * 100;
+
+    setScrollPosition(scrolled);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return scrollPosition;
+}
 
 function Tweet({
   tweet,
@@ -57,6 +85,8 @@ function Tweet({
 }
 
 export function Timeline() {
+  const scrollPostion = useScrollPosition();
+
   const { data, hasNextPage, fetchNextPage, isFetching } =
     trpc.tweet.timeline.useInfiniteQuery(
       { limit: 10 },
@@ -64,9 +94,14 @@ export function Timeline() {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
       }
     );
-  console.log("data", data);
 
   const tweets = data?.pages.flatMap((page) => page.tweets) ?? [];
+
+  useEffect(() => {
+    if (scrollPostion > 90 && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [scrollPostion, hasNextPage, isFetching, fetchNextPage]);
   return (
     <div>
       <CreateTweet />
@@ -74,13 +109,7 @@ export function Timeline() {
         {tweets.map((tweet) => {
           return <Tweet key={tweet.id} tweet={tweet} />;
         })}
-
-        <button
-          onClick={() => fetchNextPage()}
-          disabled={!hasNextPage || isFetching}
-        >
-          load next
-        </button>
+        {!hasNextPage && <p>No more tweets to load</p>}
       </div>
     </div>
   );
