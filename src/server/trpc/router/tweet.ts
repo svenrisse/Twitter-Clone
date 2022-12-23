d72ad2cc49c480a523da67d9734d3d5e7622b798
Sven <svenrisse0@gmail.com>
2022-12-23 15:14:20 +0100

    feat<trpc>: input now uses schema

--- src/server/trpc/router/tweet.ts
+++ src/server/trpc/router/tweet.ts
@@ -1,29 +1,21 @@
-import { z } from "zod";
+import { tweetSchema } from "../../../components/CreateTweet";
 import { protectedProcedure, router } from "../trpc";
 
 export const tweetRouter = router({
-  create: protectedProcedure
-    .input(
-      z.object({
-        text: z.string({
-          required_error: "Tweet text is required",
-        }),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      const { prisma, session } = ctx;
+  create: protectedProcedure.input(tweetSchema).mutation(({ ctx, input }) => {
+    const { prisma, session } = ctx;
 
-      const userId = session.user.id;
+    const userId = session.user.id;
 
-      return prisma.tweet.create({
-        data: {
-          text: input.text,
-          author: {
-            connect: {
-              id: userId,
-            },
+    return prisma.tweet.create({
+      data: {
+        text: input.text,
+        author: {
+          connect: {
+            id: userId,
           },
         },
-      });
-    }),
+      },
+    });
+  }),
 });
