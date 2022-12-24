a17652cefcfc574394320a2813504d0a69565884
Sven <svenrisse0@gmail.com>
2022-12-24 09:24:25 +0100

    feat<Timeline>: render content of tweets

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,5 +1,40 @@
-import { trpc } from "../utils/trpc";
+import Image from "next/image";
+import { RouterOutputs, trpc } from "../utils/trpc";
 import { CreateTweet } from "./CreateTweet";
+
+function Tweet({
+  tweet,
+}: {
+  tweet: RouterOutputs["tweet"]["timeline"]["tweets"][number];
+}) {
+  return (
+    <div className="mb-4 border-b-2 border-gray-500">
+      <div className="flex p-2">
+        {tweet.author.image && (
+          <Image
+            src={tweet.author.image}
+            alt={`${tweet.author.name} profile picture`}
+            width={48}
+            height={48}
+            className="rounded-full"
+          />
+        )}
+
+        <div className="ml-2">
+          <div className="flex items-center">
+            <p className="font-bold">@{tweet.author.name}</p>
+            <p className="text-sm text-gray-400">
+              {" "}
+              - {new Date(tweet.createdAt).toISOString()}
+            </p>
+          </div>
+
+          <div>{tweet.text}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export function Timeline() {
   const { data } = trpc.tweet.timeline.useQuery({});
@@ -7,7 +42,12 @@ export function Timeline() {
   return (
     <div>
       <CreateTweet />
-      {JSON.stringify(data)}
+
+      <div className="border-l-2 border-r-2 border-t-2 border-gray-500">
+        {data?.tweets.map((tweet) => {
+          return <Tweet key={tweet.id} tweet={tweet} />;
+        })}
+      </div>
     </div>
   );
 }
