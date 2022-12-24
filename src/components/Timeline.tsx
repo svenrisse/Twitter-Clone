f6eaad7ce00d53b8099a26a6d8951330e881d083
Sven <svenrisse0@gmail.com>
2022-12-24 09:05:48 +0100

    feat: add render of tweets

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,9 +1,13 @@
+import { trpc } from "../utils/trpc";
 import { CreateTweet } from "./CreateTweet";
 
 export function Timeline() {
+  const { data } = trpc.tweet.timeline.useQuery({});
+
   return (
     <div>
       <CreateTweet />
+      {JSON.stringify(data)}
     </div>
   );
 }
