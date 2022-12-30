1b24e1e32548e0fc11b07788e795956c63129cd0
Sven <svenrisse0@gmail.com>
2022-12-30 13:51:52 +0100

    feat: add userRouter to appRouter

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { tweetRouter } from "./tweet";
+import { userRouter } from "./user";
 
 export const appRouter = router({
   tweet: tweetRouter,
   auth: authRouter,
+  user: userRouter,
 });
 
 // export type definition of API
