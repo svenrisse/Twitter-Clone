ebb43730ffa71ead02fb8f223511b99156ff64f7
Sven <svenrisse0@gmail.com>
2022-12-30 13:50:32 +0100

    feat: add user router

---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { publicProcedure, router } from "../trpc";
+export const userRouter = router({
+  getUser: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { id } = input;
+      return await ctx.prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+    }),
+});
