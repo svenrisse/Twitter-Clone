6b9704d3bf4af28510e4675a2616b5916d16df5c
Sven <svenrisse0@gmail.com>
2022-12-31 11:35:53 +0100

    feat: add basic profile stats

--- src/pages/[name].tsx
+++ src/pages/[name].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { Timeline } from "../components/Timeline";
 import { trpc } from "../utils/trpc";
+import Image from "next/image";
 
 export default function UserPage() {
   const router = useRouter();
@@ -12,7 +13,25 @@ export default function UserPage() {
   });
   return (
     <div>
-      {data?.image}
+      <div className="pt-10">
+        <div className="ml-3">
+          <Image
+            src={data?.image as string}
+            alt={`${data?.name} profile image`}
+            height={100}
+            width={100}
+          />
+        </div>
+        <div className="pt-5">
+          <h2>@{data?.name}</h2>
+          <div className="flex w-1/2 justify-between">
+            <span>0 Following</span>
+            <span>0 Followers</span>
+            <span>{data?.tweet.length} Tweets</span>
+            <span>{data?.likes.length} Likes given</span>
+          </div>
+        </div>
+      </div>
       <Timeline
         where={{
           author: {
