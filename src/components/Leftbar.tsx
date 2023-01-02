e74d3203641bc83fd0400d2948470ca4bfc55aae
Sven <svenrisse0@gmail.com>
2023-01-02 16:06:31 +0100

    feat: add leftbar buttons

--- src/components/Leftbar.tsx
+++ src/components/Leftbar.tsx
@@ -1,8 +1,17 @@
 import Link from "next/link";
-import { AiOutlineTwitter } from "react-icons/ai";
+import {
+  AiFillBell,
+  AiFillHome,
+  AiFillMessage,
+  AiOutlineTwitter,
+  AiOutlineUnorderedList,
+} from "react-icons/ai";
+import { BsFillBookmarkFill, BsFillPersonFill, BsHash } from "react-icons/bs";
+import { CiCircleMore } from "react-icons/ci";
+
 export default function Leftbar() {
   return (
-    <div className="fixed left-0 flex w-1/4 justify-end px-12">
+    <div className="fixed left-0 flex w-1/4 flex-col  justify-between space-y-4 px-12 py-16 text-lg">
       <div>
         <Link href={"/"}>
           <AiOutlineTwitter
@@ -11,6 +20,38 @@ export default function Leftbar() {
             className="hover:fill-cyan-500"
           />
         </Link>
+      </div>
+      <div className="flex items-center ">
+        <AiFillHome size={"2.5rem"} />
+        <span className="px-6">Home</span>
+      </div>
+      <div className="flex items-center ">
+        <BsHash size={"2.5rem"} />
+        <span className="px-6">Explore</span>
+      </div>
+      <div className="flex items-center ">
+        <AiFillBell size={"2.5rem"} />
+        <span className="px-6">Notifications</span>
+      </div>
+      <div className="flex items-center ">
+        <AiFillMessage size={"2.5rem"} />
+        <span className="px-6">Messages</span>
+      </div>
+      <div className="flex items-center ">
+        <BsFillBookmarkFill size={"2.5rem"} />
+        <span className="px-6">Bookmarks</span>
+      </div>
+      <div className="flex items-center ">
+        <AiOutlineUnorderedList size={"2.5rem"} />
+        <span className="px-6">Lists</span>
+      </div>
+      <div className="flex items-center ">
+        <BsFillPersonFill size={"2.5rem"} />
+        <span className="px-6">Profile</span>
+      </div>
+      <div className="flex items-center ">
+        <CiCircleMore size={"2.5rem"} />
+        <span className="px-6">More</span>
       </div>
     </div>
   );
