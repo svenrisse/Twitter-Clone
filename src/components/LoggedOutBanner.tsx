f22c3df9feb29d7f05ab885a1325f466db8ba3b5
Sven <svenrisse0@gmail.com>
2023-01-05 12:32:18 +0100

    style: update styling of LoggedOutBanner

--- src/components/LoggedOutBanner.tsx
+++ src/components/LoggedOutBanner.tsx
@@ -7,15 +7,17 @@ export function LoggedOutBanner() {
     return null;
   }
   return (
-    <div className="w- fixed bottom-0 flex w-full justify-center bg-primary p-4">
+    <div className="w- fixed bottom-0 flex w-full justify-center bg-slate-500 p-4">
       <Container
-        classNames="bg-transparent flex justify-between w-1/2
+        classNames="bg-transparent flex justify-between items-center w-1/2
       "
       >
-        <p className="text-center">Do not miss out</p>
+        <p className="text-center text-xl font-bold">
+          Please login to get access to all features!
+        </p>
         <div>
           <button
-            className="px-4 py-2 text-white shadow-md"
+            className="w-40 rounded-full bg-cyan-500 px-4 py-2 text-xl text-white shadow-md"
             onClick={() => signIn()}
           >
             Login
