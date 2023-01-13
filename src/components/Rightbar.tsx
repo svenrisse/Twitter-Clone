67ba3196b64034e10dce4d7a3712ce5dc43fbd6e
Sven <svenrisse0@gmail.com>
2023-01-13 13:12:34 +0100

    feat: add rightbar dummy content

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,7 +1,36 @@
 export default function Rightbar() {
   return (
-    <div className="fixed right-0 flex w-1/4 justify-start">
-      <div>This is the rightbar</div>
+    <div className="fixed right-0 flex w-1/4 justify-center px-12 py-16">
+      <div className="rounded-xl bg-slate-300 px-4 py-6">
+        <h1 className="text-xl font-extrabold">Trends for you</h1>
+        <div className="pr-20">
+          <div className="py-5">
+            <div className="text-sm text-gray-600">Trending</div>
+            <div className="font-bold">#ExampleTrend</div>
+            <div className="text-sm text-gray-600">13K Tweets</div>
+          </div>
+          <div className="py-5">
+            <div className="text-sm text-gray-600">Trending</div>
+            <div className="font-bold">#AnotherTrend</div>
+            <div className="text-sm text-gray-600">7K Tweets</div>
+          </div>
+          <div className="py-5">
+            <div className="text-sm text-gray-600">Trending</div>
+            <div className="font-bold">#FunnyTrend</div>
+            <div className="text-sm text-gray-600">53K Tweets</div>
+          </div>
+          <div className="py-5">
+            <div className="text-sm text-gray-600">Trending</div>
+            <div className="font-bold">#BoringTrend</div>
+            <div className="text-sm text-gray-600">606K Tweets</div>
+          </div>
+          <div className="py-5">
+            <div className="text-sm text-gray-600">Trending</div>
+            <div className="font-bold">#FoodTrend</div>
+            <div className="text-sm text-gray-600">2K Tweets</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
