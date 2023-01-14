c8d7df9f9ebba6b91bb25a290184641dac757986
Sven <svenrisse0@gmail.com>
2023-01-14 10:58:22 +0100

    feat: add liked page

---
+++ src/pages/liked.tsx
@@ -0,0 +1,24 @@
+import { useQueryClient } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+import Leftbar from "../components/Leftbar";
+import Rightbar from "../components/Rightbar";
+import { Tweet } from "../components/Timeline";
+import { trpc } from "../utils/trpc";
+export default function Liked() {
+  const { data: session } = useSession();
+
+  const { data } = trpc.user.getLikes.useQuery({
+    id: session?.user?.id,
+  });
+  const client = useQueryClient();
+
+  console.log(data);
+  return (
+    <div className="flex min-h-screen justify-center">
+      <Leftbar />
+      <div>{session?.user?.id}</div>
+
+      <Rightbar />
+    </div>
+  );
+}
