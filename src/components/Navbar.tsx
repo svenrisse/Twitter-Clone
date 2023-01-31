cfa2cd7c1950ddd0f965391fcddfbb39cd08e7f5
Sven <svenrisse0@gmail.com>
2023-01-31 23:56:39 +0100

    style: base style for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,65 +15,52 @@ export default function Navbar({
   const { data, status } = useSession();
 
   return (
-    <div className="fixed top-0 right-0 flex w-1/4 justify-center ">
-      <div className="flex space-y-4 px-12 py-16 text-lg">
-        <div className="w-fit">
-          <Link href={"/"}>
-            <div className="group flex items-center rounded-md hover:bg-slate-300">
-              <AiOutlineTwitter
-                size={"2.5rem"}
-                className={
-                  focusedHome ? "fill-cyan-500" : "group-hover:fill-cyan-500"
-                }
-              />
-              <span className={focusedHome ? "px-6 font-bold" : "px-6"}>
-                Home
-              </span>
-            </div>
-          </Link>
-        </div>
-        <div className="w-fit">
-          <Link href={"/liked"}>
-            <div className="group flex items-center rounded-md hover:bg-slate-300">
-              <AiFillHeart
-                size={"2.5rem"}
-                className={
-                  focusedLiked ? "fill-cyan-500" : "group-hover:fill-cyan-500"
-                }
-              />
-              <span className={focusedLiked ? "px-6 font-bold" : "px-6"}>
-                Liked
-              </span>
-            </div>
-          </Link>
-        </div>
-        {status === "authenticated" && (
-          <div className="flex items-center ">
-            <Link
-              href={{
-                pathname: `${data?.user?.name}`,
-                query: {
-                  id: data?.user?.id,
-                },
-              }}
-            >
-              <div className="group flex items-center rounded-md hover:bg-slate-300">
-                <BsFillPersonFill
-                  size={"2.5rem"}
-                  className={
-                    focusedProfile
-                      ? "fill-cyan-500"
-                      : "group-hover:fill-cyan-500"
-                  }
-                />
-                <span className={focusedProfile ? "px-6 font-bold" : "px-6"}>
-                  Profile
-                </span>
-              </div>
-            </Link>
+    <div className="fixed top-0 right-0 flex justify-center text-lg">
+      <div className="w-fit">
+        <Link href={"/"}>
+          <div className="group flex items-center rounded-md hover:bg-slate-300">
+            <AiOutlineTwitter
+              size={"2.5rem"}
+              className={
+                focusedHome ? "fill-cyan-500" : "group-hover:fill-cyan-500"
+              }
+            />
           </div>
-        )}
+        </Link>
       </div>
+      <div className="w-fit">
+        <Link href={"/liked"}>
+          <div className="group flex items-center rounded-md hover:bg-slate-300">
+            <AiFillHeart
+              size={"2.5rem"}
+              className={
+                focusedLiked ? "fill-cyan-500" : "group-hover:fill-cyan-500"
+              }
+            />
+          </div>
+        </Link>
+      </div>
+      {status === "authenticated" && (
+        <div className="flex items-center ">
+          <Link
+            href={{
+              pathname: `${data?.user?.name}`,
+              query: {
+                id: data?.user?.id,
+              },
+            }}
+          >
+            <div className="group flex items-center rounded-md hover:bg-slate-300">
+              <BsFillPersonFill
+                size={"2.5rem"}
+                className={
+                  focusedProfile ? "fill-cyan-500" : "group-hover:fill-cyan-500"
+                }
+              />
+            </div>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
