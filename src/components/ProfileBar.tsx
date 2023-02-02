b04e4e08632c2a939c305db01d17c3b32424f2aa
Sven <svenrisse0@gmail.com>
2023-02-02 14:01:14 +0100

    feat: update ProfileBar for lg+

--- src/components/ProfileBar.tsx
+++ src/components/ProfileBar.tsx
@@ -2,6 +2,7 @@ import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useState } from "react";
 import Modal from "react-modal";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 export default function ProfileBar() {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -21,8 +22,11 @@ export default function ProfileBar() {
   }
 
   return (
-    <div>
-      <div className="cursor-pointer rounded-full px-8" onClick={openModal}>
+    <div className="lg:fixed lg:bottom-4">
+      <div
+        className="cursor-pointer rounded-full px-8 lg:flex lg:items-center lg:gap-2 lg:bg-slate-300 lg:py-2"
+        onClick={openModal}
+      >
         <Image
           src={data.user?.image as string}
           alt={`${data.user?.name} profile picture`}
@@ -30,11 +34,13 @@ export default function ProfileBar() {
           height={50}
           className="rounded-full"
         />
+        <span className="hidden lg:block lg:font-bold">{data.user?.name}</span>
+        <BsThreeDotsVertical className="hidden lg:block" size={"1.5rem"} />
       </div>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
-        className="fixed bottom-20 left-2 border-0"
+        className="fixed bottom-20 left-2 border-0 lg:left-28 lg:bottom-24"
       >
         <button
           className="w-20 rounded-xl bg-cyan-700 py-2"
