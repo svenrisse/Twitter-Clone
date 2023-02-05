7ca5d1f3872501a0c2b43816a6055f10a165fdd7
Sven <svenrisse0@gmail.com>
2023-02-05 11:16:20 +0100

    feat: add link to ProfileBar picture

--- src/components/ProfileBar.tsx
+++ src/components/ProfileBar.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import Modal from "react-modal";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import Link from "next/link";
 
 export default function ProfileBar() {
   const [modalIsOpen, setIsOpen] = useState(false);
@@ -27,13 +28,22 @@ export default function ProfileBar() {
         className="cursor-pointer rounded-full px-8 lg:flex lg:items-center lg:gap-2 lg:bg-slate-300 lg:py-2"
         onClick={openModal}
       >
-        <Image
-          src={data.user?.image as string}
-          alt={`${data.user?.name} profile picture`}
-          width={50}
-          height={50}
-          className="rounded-full"
-        />
+        <Link
+          href={{
+            pathname: `${data.user?.name}`,
+            query: {
+              id: data.user?.id,
+            },
+          }}
+        >
+          <Image
+            src={data.user?.image as string}
+            alt={`${data.user?.name} profile picture`}
+            width={50}
+            height={50}
+            className="rounded-full"
+          />
+        </Link>
         <span className="hidden lg:block lg:font-bold">{data.user?.name}</span>
         <BsThreeDotsVertical className="hidden lg:block" size={"1.5rem"} />
       </div>
