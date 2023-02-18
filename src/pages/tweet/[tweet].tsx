7dcb36544f726616d2ef639e7eb7276e8c8c6c30
Sven <svenrisse0@gmail.com>
2023-02-18 21:00:13 +0100

    style(tweetPage): center tweet

--- src/pages/tweet/[tweet].tsx
+++ src/pages/tweet/[tweet].tsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar";
 import Rightbar from "../../components/Rightbar";
 import { Tweet } from "../../components/Timeline";
 import { trpc } from "../../utils/trpc";
+import { ThreeDots } from "react-loader-spinner";
 
 export default function TweetPage() {
   const id = useRouter().asPath.split("/")[2] as string;
@@ -12,14 +13,21 @@ export default function TweetPage() {
   });
 
   return (
-    <div className="min-h-screen bg-slate-200">
+    <>
       <Navbar />
       <Rightbar />
-      {data && (
-        <div>
-          <Tweet tweet={data} />
-        </div>
-      )}
-    </div>
+      <div className="flex min-h-screen flex-col items-center">
+        <h1 className="w-screen rounded-b-md bg-slate-600 py-2 text-center font-mono text-xl font-medium uppercase text-slate-200">
+          Tweets you liked
+        </h1>
+        {isFetching ? (
+          <ThreeDots color="cyan" height="100" />
+        ) : (
+          <div className="mt-12 mb-20 h-max w-11/12 rounded-xl border-l-2 border-r-2 border-t-2 border-slate-400 lg:w-1/2 2xl:w-5/12">
+            <Tweet tweet={data} />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
