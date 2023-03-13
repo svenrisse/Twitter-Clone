9cceb636b9fd419e22d614a08044290002fd2dc5
Sven <svenrisse0@gmail.com>
2023-03-13 11:20:15 +0100

    adjust for mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar({ focused }: { focused?: string }) {
   return (
     <div className="fixed bottom-0 flex w-screen items-center rounded-t-md bg-slate-700 py-2 lg:left-14 lg:top-40 lg:h-56 lg:w-2/12 lg:rounded-xl lg:bg-slate-200 2xl:top-36 2xl:left-72 2xl:w-16">
       <ProfileBar />
-      <div className="ml-auto flex justify-center gap-5 px-8 text-lg lg:ml-0 lg:mb-auto lg:flex-col lg:items-center 2xl:text-xl">
+      <div className="ml-auto flex justify-center px-4 text-lg md:gap-5 lg:ml-0 lg:mb-auto lg:flex-col lg:items-center 2xl:text-xl">
         <div className="w-fit lg:self-start">
           <Link href={"/"}>
             <div className="group flex items-center gap-5 rounded-md px-3 hover:bg-slate-300">
@@ -22,7 +22,9 @@ export default function Navbar({ focused }: { focused?: string }) {
                     : "group-hover:fill-cyan-500"
                 }
               />
-              <span className={focused == "home" ? "font-bold" : ""}>Home</span>
+              <span className={`${focused && "font-bold"} hidden md:block`}>
+                Home
+              </span>
             </div>
           </Link>
         </div>
@@ -38,7 +40,7 @@ export default function Navbar({ focused }: { focused?: string }) {
                       : "group-hover:fill-cyan-500"
                   }
                 />
-                <span className={focused == "liked" ? "font-bold" : ""}>
+                <span className={`${focused && "font-bold"} hidden md:block`}>
                   Liked
                 </span>
               </div>
@@ -64,7 +66,7 @@ export default function Navbar({ focused }: { focused?: string }) {
                       : "group-hover:fill-cyan-500"
                   }
                 />
-                <span className={focused == "profile" ? "font-bold" : ""}>
+                <span className={`${focused && "font-bold"} hidden md:block`}>
                   Profile
                 </span>
               </div>
