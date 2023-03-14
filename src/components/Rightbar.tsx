44263355de84a3bd9a5fa920d7ed8975cdbdb518
Sven <svenrisse0@gmail.com>
2023-03-14 10:18:55 +0100

    style(Rightbar): adjust for laptop

--- src/components/Rightbar.tsx
+++ src/components/Rightbar.tsx
@@ -1,31 +1,26 @@
 export default function Rightbar() {
   return (
-    <div className="fixed right-0 top-40 hidden w-1/4 justify-center lg:flex 2xl:right-32 2xl:top-36">
+    <div className="fixed top-28 right-10 hidden w-2/12 justify-center lg:flex 2xl:right-32 2xl:top-36">
       <div className="rounded-xl bg-slate-300 px-4 py-6">
-        <h1 className="text-xl font-extrabold">Trends for you</h1>
-        <div className="pr-20">
-          <div className="py-5">
-            <div className="text-sm text-gray-600">Trending</div>
+        <h1 className="py-4 text-xl font-extrabold">Trends for you</h1>
+        <div className="pr-10">
+          <div className="py-2">
             <div className="font-bold">#ExampleTrend</div>
             <div className="text-sm text-gray-600">13K Tweets</div>
           </div>
-          <div className="py-5">
-            <div className="text-sm text-gray-600">Trending</div>
+          <div className="py-2">
             <div className="font-bold">#AnotherTrend</div>
             <div className="text-sm text-gray-600">7K Tweets</div>
           </div>
-          <div className="py-5">
-            <div className="text-sm text-gray-600">Trending</div>
+          <div className="py-2">
             <div className="font-bold">#FunnyTrend</div>
             <div className="text-sm text-gray-600">53K Tweets</div>
           </div>
-          <div className="py-5">
-            <div className="text-sm text-gray-600">Trending</div>
+          <div className="py-2">
             <div className="font-bold">#BoringTrend</div>
             <div className="text-sm text-gray-600">606K Tweets</div>
           </div>
-          <div className="py-5">
-            <div className="text-sm text-gray-600">Trending</div>
+          <div className="py-2">
             <div className="font-bold">#FoodTrend</div>
             <div className="text-sm text-gray-600">2K Tweets</div>
           </div>
