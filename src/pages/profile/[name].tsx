adbb807e110872102f7a0d2242cc664a65ddc1b9
Sven <svenrisse0@gmail.com>
2023-03-15 12:49:37 +0100

    fix(userPage): dont render follow button on own profile

--- src/pages/profile/[name].tsx
+++ src/pages/profile/[name].tsx
@@ -170,42 +170,44 @@ export default function UserPage() {
               </div>
             </div>
           </div>
-          <div className="mt-3">
-            <button
-              onClick={(e) => handleFollowClick(e)}
-              className={`h-12 w-28 rounded-md bg-primary px-4 py-2 font-bold text-white active:bg-blue-600 ${
-                hasFollow && "hover:bg-red-500"
-              }`}
-              disabled={followLoading || unfollowLoading}
-            >
-              {followLoading || unfollowLoading ? (
-                <div className="flex items-center justify-center">
-                  <svg
-                    className="h-5 w-5 animate-spin text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      stroke-width="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
-                </div>
-              ) : (
-                <div>{hasFollow ? "Following" : "Follow"}</div>
-              )}
-            </button>
-          </div>
+          {data?.id !== userId && (
+            <div className="mt-3">
+              <button
+                onClick={(e) => handleFollowClick(e)}
+                className={`h-12 w-28 rounded-md bg-primary px-4 py-2 font-bold text-white active:bg-blue-600 ${
+                  hasFollow && "hover:bg-red-500"
+                }`}
+                disabled={followLoading || unfollowLoading}
+              >
+                {followLoading || unfollowLoading ? (
+                  <div className="flex items-center justify-center">
+                    <svg
+                      className="h-5 w-5 animate-spin text-white"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                      ></circle>
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      ></path>
+                    </svg>
+                  </div>
+                ) : (
+                  <div>{hasFollow ? "Following" : "Follow"}</div>
+                )}
+              </button>
+            </div>
+          )}
           <Timeline
             where={{
               author: {
